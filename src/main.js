9a2636d9a2d461187d4d33158b87e83ff44c4562
YUUKIToriyama <github@toriyama.dev>
2021-10-31 18:04:32 +0900

    スタイルファイルを@blogmd/stylesから読み込んで使うように変更

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 const BlogMd = require("@blogmd/core").default;
+const { Basic } = require("@blogmd/styles");
+
+const [style, template] = Basic.useStyle(); // @blogmd/styles/basicを用いる
 
 const blogmd = new BlogMd({
 	articleDir: "./articles",
 	outDir: "./public",
-	styleFile: "./src/basic.scss"
+	styleFile: style,
+	template: template
 });
 blogmd.build();
 blogmd.buildIndex();
